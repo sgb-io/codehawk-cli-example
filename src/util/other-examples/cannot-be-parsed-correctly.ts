33acd6cd1d3c993617b42e2bca02604391242804
sgb-io <sam@sgb.io>
2021-01-06 19:23:55 +0000

    Upgrade to codehawk-cli 8.1.0, example of code that will be skipped

---
+++ src/util/other-examples/cannot-be-parsed-correctly.ts
@@ -0,0 +1,10 @@
+// Yes.. this is java! It's intentional - to demonstrate how codehawk-cli can avoid certain files.
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+
+}
